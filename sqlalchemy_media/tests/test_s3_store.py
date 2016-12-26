9533f38cfd5f3f0647189134f9397104069776d3
YukSeungChan <loup1788@gmail.com>
2016-12-26 18:20:18 +0900

    add error tests.

--- sqlalchemy_media/tests/test_s3_store.py
+++ sqlalchemy_media/tests/test_s3_store.py
@@ -15,19 +15,19 @@ from sqlalchemy_media.stores import S3Store
 from sqlalchemy_media.stores import StoreManager
 from sqlalchemy_media.tests.helpers import Json, SqlAlchemyTestCase
 
-TEST_HOST = '127.0.0.1'
+TEST_HOST = 'localhost'
 TEST_PORT = 10002
-TEST_BUCKET = '127'
+TEST_BUCKET = 'test-bucket'
 TEST_ACCESS_KEY = 'test_access_key'
 TEST_SECRET_KEY = 'test_secret_key'
 TEST_REGION = 'ap-northeast-2'
-TEST_BASE_URL_FORMAT = 'http://{0}.%s:%d' % (TEST_HOST[4:], TEST_PORT)
+TEST_BASE_URL_FORMAT = 'http://%s:%d/{0}' % (TEST_HOST, TEST_PORT)
 TEST_SERVER_URL = TEST_BASE_URL_FORMAT.format(TEST_BUCKET)
 
 
-def _get_s3_store(**kwargs):
+def _get_s3_store(bucket=TEST_BUCKET, **kwargs):
     S3Store.BASE_URL_FORMAT = TEST_BASE_URL_FORMAT
-    store = S3Store(TEST_BUCKET, TEST_ACCESS_KEY, TEST_SECRET_KEY, TEST_REGION,
+    store = S3Store(bucket, TEST_ACCESS_KEY, TEST_SECRET_KEY, TEST_REGION,
                     **kwargs)
     return store
 
@@ -71,6 +71,15 @@ class S3StoreTestCase(SqlAlchemyTestCase):
         self.assertEqual(length, len(content))
         self.assertIsInstance(store.open(target_filename), io.BytesIO)
 
+    def test_put_error(self):
+        store = _get_s3_store(bucket='{0}-2'.format(TEST_BUCKET))
+        target_filename = 'test_put_from_stream/file_from_stream1.txt'
+        content = b'Lorem ipsum dolor sit amet'
+        stream = io.BytesIO(content)
+
+        with self.assertRaises(S3Error):
+            store.put(target_filename, stream)
+
     def test_delete(self):
         store = _get_s3_store()
         target_filename = 'test_delete/sample_text_file1.txt'
@@ -83,6 +92,18 @@ class S3StoreTestCase(SqlAlchemyTestCase):
 
         with self.assertRaises(S3Error):
             store.open(target_filename)
+
+    def test_delete_error(self):
+        store = _get_s3_store()
+        wrong_store = _get_s3_store(bucket='{0}-2'.format(TEST_BUCKET))
+        target_filename = 'test_delete/sample_text_file1.txt'
+        with open(self.sample_text_file1, 'rb') as f:
+            length = store.put(target_filename, f)
+        self.assertEqual(length, getsize(self.sample_text_file1))
+        self.assertIsInstance(store.open(target_filename), io.BytesIO)
+
+        with self.assertRaises(S3Error):
+            wrong_store.delete(target_filename)
 
     def test_open(self):
         store = _get_s3_store()
@@ -147,6 +168,41 @@ class S3StoreTestCase(SqlAlchemyTestCase):
             self.assertIsInstance(person1.file, File)
             self.assertEqual(person1.file.locate(), '%s/%s?_ts=%s' % (
                 public_base_url, person1.file.path, person1.file.timestamp))
+
+    def test_public_base_url_strip(self):
+        public_base_url = 'http://test.sqlalchemy.media/'
+        StoreManager.register(
+            's3',
+            lambda: _get_s3_store(public_base_url=public_base_url),
+            default=True
+        )
+
+        class Person(self.Base):
+            __tablename__ = 'person'
+            id = Column(Integer, primary_key=True)
+            file = Column(File.as_mutable(Json))
+
+        session = self.create_all_and_get_session()
+
+        person1 = Person()
+        self.assertIsNone(person1.file)
+        sample_content = b'Simple text.'
+
+        with StoreManager(session):
+            person1 = Person()
+            person1.file = File.create_from(io.BytesIO(sample_content),
+                                            content_type='text/plain',
+                                            extension='.txt')
+            self.assertIsInstance(person1.file, File)
+            self.assertEqual(
+                person1.file.locate(),
+                '%s/%s?_ts=%s' % (
+                    public_base_url.rstrip('/'),
+                    person1.file.path,
+                    person1.file.timestamp
+                )
+            )
+
 
 if __name__ == '__main__':  # pragma: no cover
     unittest.main()
