676424eaccf5ec3d61eab53145fb6c90e8be3e16
Vahid <vahid.mardani@gmail.com>
2017-06-05 20:28:05 +0430

    Migrating OS2 test_public_base_url to new mockup server, #83

--- sqlalchemy_media/tests/test_os2_store.py
+++ sqlalchemy_media/tests/test_os2_store.py
@@ -134,9 +134,13 @@ class OS2StoreTestCase(SqlAlchemyTestCase):
     def test_prefix(self):
         with mockup_os2_server(self.temp_path, TEST_BUCKET) as (server, url):
             prefix = 'test'
-            StoreManager.register('os2', functools.partial(
-                create_os2_store, server, base_url=url, prefix=prefix
-            ), default=True)
+            StoreManager.register(
+                'os2',
+                functools.partial(
+                    create_os2_store, server, base_url=url, prefix=prefix
+                ),
+                default=True
+            )
 
             class Person(self.Base):
                 __tablename__ = 'person'
@@ -162,34 +166,34 @@ class OS2StoreTestCase(SqlAlchemyTestCase):
                     person1.file.timestamp
                 ))
 
-    # def test_public_base_url(self):
-    #     public_base_url = 'http://test.sqlalchemy.media'
-    #     StoreManager.register(
-    #         's3',
-    #         lambda: _get_os2_store(public_base_url=public_base_url),
-    #         default=True
-    #     )
-    #
-    #     class Person(self.Base):
-    #         __tablename__ = 'person'
-    #         id = Column(Integer, primary_key=True)
-    #         file = Column(File.as_mutable(Json))
-    #
-    #     session = self.create_all_and_get_session()
-    #
-    #     person1 = Person()
-    #     self.assertIsNone(person1.file)
-    #     sample_content = b'Simple text.'
-    #
-    #     with StoreManager(session):
-    #         person1 = Person()
-    #         person1.file = File.create_from(io.BytesIO(sample_content),
-    #                                         content_type='text/plain',
-    #                                         extension='.txt')
-    #         self.assertIsInstance(person1.file, File)
-    #         self.assertEqual(person1.file.locate(), '%s/%s?_ts=%s' % (
-    #             public_base_url, person1.file.path, person1.file.timestamp))
-    #
+    def test_public_base_url(self):
+        with mockup_os2_server(self.temp_path, TEST_BUCKET) as (server, url):
+            StoreManager.register(
+                'os2',
+                functools.partial(create_os2_store, server, base_url=url),
+                default=True
+            )
+
+            class Person(self.Base):
+                __tablename__ = 'person'
+                id = Column(Integer, primary_key=True)
+                file = Column(File.as_mutable(Json))
+
+            session = self.create_all_and_get_session()
+
+            person1 = Person()
+            self.assertIsNone(person1.file)
+            sample_content = b'Simple text.'
+
+            with StoreManager(session):
+                person1 = Person()
+                person1.file = File.create_from(io.BytesIO(sample_content),
+                                                content_type='text/plain',
+                                                extension='.txt')
+                self.assertIsInstance(person1.file, File)
+                self.assertEqual(person1.file.locate(), '%s/%s?_ts=%s' % (
+                    url, person1.file.path, person1.file.timestamp))
+
     # def test_public_base_url_strip(self):
     #     public_base_url = 'http://test.sqlalchemy.media/'
     #     StoreManager.register(
