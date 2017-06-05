77ca2bad71d4b908e6edec3a5009106a6d34aca1
Vahid <vahid.mardani@gmail.com>
2017-06-05 17:59:29 +0430

    Migrating S3 test_locate to new mockup server, #82

--- sqlalchemy_media/tests/test_s3_store.py
+++ sqlalchemy_media/tests/test_s3_store.py
@@ -136,29 +136,30 @@ class S3StoreTestCase(SqlAlchemyTestCase):
             with store.open(target_filename, mode='rb') as stored_file, \
                     open(self.sample_text_file1, mode='rb') as original_file:
                 self.assertEqual(stored_file.read(), original_file.read())
-    #
-    # def test_locate(self):
-    #     StoreManager.register('s3', _get_s3_store, default=True)
-    #
-    #     class Person(self.Base):
-    #         __tablename__ = 'person'
-    #         id = Column(Integer, primary_key=True)
-    #         file = Column(File.as_mutable(Json))
-    #
-    #     session = self.create_all_and_get_session()
-    #
-    #     person1 = Person()
-    #     self.assertIsNone(person1.file)
-    #     sample_content = b'Simple text.'
-    #
-    #     with StoreManager(session):
-    #         person1 = Person()
-    #         person1.file = File.create_from(io.BytesIO(sample_content),
-    #                                         content_type='text/plain',
-    #                                         extension='.txt')
-    #         self.assertIsInstance(person1.file, File)
-    #         self.assertEqual(person1.file.locate(), '%s/%s?_ts=%s' % (
-    #             TEST_SERVER_URL, person1.file.path, person1.file.timestamp))
+
+    def test_locate(self):
+        with mockup_s3_server(TEST_BUCKET) as (server, uri):
+            StoreManager.register('s3', functools.partial(create_s3_store, base_url=uri), default=True)
+
+            class Person(self.Base):
+                __tablename__ = 'person'
+                id = Column(Integer, primary_key=True)
+                file = Column(File.as_mutable(Json))
+
+            session = self.create_all_and_get_session()
+
+            person1 = Person()
+            self.assertIsNone(person1.file)
+            sample_content = b'Simple text.'
+
+            with StoreManager(session):
+                person1 = Person()
+                person1.file = File.create_from(io.BytesIO(sample_content),
+                                                content_type='text/plain',
+                                                extension='.txt')
+                self.assertIsInstance(person1.file, File)
+                self.assertEqual(person1.file.locate(), '%s/%s?_ts=%s' % (
+                    uri, person1.file.path, person1.file.timestamp))
     #
     # def test_prefix(self):
     #     prefix = 'test'
