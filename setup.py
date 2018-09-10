8647c80eaae6cb2b64e8cf42db17a58de34a8ad2
Jean-Philippe Morin <jpmorin196@gmail.com>
2018-09-10 08:07:20 -0400

    add dependency to python-magic-bin for windows and mac

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ with open(os.path.join(
 dependencies = [
     'sqlalchemy >= 1.1.0b3',
     'pillow',
-    'python-magic >= 0.4.12'
+    'python-magic >= 0.4.12',
+    'python-magic-bin >= 0.4.12; (sys_platform=="win32" or sys_platform=="darwin")'
 ]
 
 
