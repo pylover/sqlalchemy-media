26b3c955d537b83ce547c5f92813f869169c5bc6
pylover <vahid.mardani@gmail.com>
2019-01-17 02:11:51 +0330

    v0.17.3

--- sqlalchemy_media/__init__.py
+++ sqlalchemy_media/__init__.py
@@ -10,4 +10,4 @@ from .processors import Processor, ImageProcessor, Analyzer, MagicAnalyzer, \
     Validator, ContentTypeValidator, ImageValidator, ImageAnalyzer
 
 
-__version__ = '0.17.2'
+__version__ = '0.17.3'
