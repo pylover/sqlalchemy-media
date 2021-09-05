f9bfc744e173b7efa86b00e9279602326b69750e
TheLazzziest <mxyakovenko9@gmail.com>
2021-09-05 14:50:47 +0300

    Add imports from config and s3 helpers

--- sqlalchemy_media/tests/helpers/__init__.py
+++ sqlalchemy_media/tests/helpers/__init__.py
@@ -1,7 +1,7 @@
-
+from .config import TEST_ACCESS_KEY, TEST_BUCKET, TEST_REGION, TEST_SECRET_KEY
 from .http import simple_http_server, encode_multipart_data
 from .os2 import mockup_os2_server
-from .s3 import mockup_s3_server
+from .s3 import mockup_s3_server, create_s3_store, S3TestCase
 from .ssh import MockupSSHServer, MockupSSHTestCase
 from .ftp import MockFTP
 from .static import mockup_http_static_server
