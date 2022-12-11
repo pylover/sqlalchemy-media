a54cd485803ae6f02fb2e73b23d49e4a8fda5a4b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-12-11 09:50:55 -0500

    fix mocking paths

--- sqlalchemy_media/tests/test_google_cloud_storage.py
+++ sqlalchemy_media/tests/test_google_cloud_storage.py
@@ -13,7 +13,7 @@ FAKE_REGION = 'fake-region'
 
 
 class TestGoogleCloudStorgeObject(SqlAlchemyTestCase):
-    @mock.patch('sqlalchemy_media.stores.gcp.storage')
+    @mock.patch('sqlalchemy_media.stores.GoogleCloudStorage.storage')
     def test_upload_file(self, mock_storage: storage):
         mock_storage.Client.return_value = Mock()
         mock_storage.Client.return_value.get_bucket.return_value = Mock()
@@ -35,7 +35,7 @@ class TestGoogleCloudStorgeObject(SqlAlchemyTestCase):
             mock_storage.Client.from_service_account_json.return_value.get_bucket.return_value.blob.return_value. \
                 upload_from_string.assert_called_once()
 
-    @mock.patch('sqlalchemy_media.stores.gcp.storage')
+    @mock.patch('sqlalchemy_media.stores.GoogleCloudStorage.storage')
     def test_get_url(self, mock_storage: storage):
         mock_storage.Client.return_value = Mock()
         mock_storage.Client.return_value.get_bucket.return_value = Mock()
@@ -52,7 +52,7 @@ class TestGoogleCloudStorgeObject(SqlAlchemyTestCase):
         mock_storage.Client.from_service_account_json.return_value.get_bucket.return_value.blob \
             .assert_called_with('cat.jpg')
 
-    @mock.patch('sqlalchemy_media.stores.gcp.storage')
+    @mock.patch('sqlalchemy_media.stores.GoogleCloudStorage.storage')
     def test_open(self, mock_storage: storage):
         mock_storage.Client.return_value = Mock()
         mock_storage.Client.return_value.get_bucket.return_value = Mock()
@@ -69,7 +69,7 @@ class TestGoogleCloudStorgeObject(SqlAlchemyTestCase):
         mock_storage.Client.from_service_account_json.return_value.get_bucket.return_value.blob \
             .assert_called_with('cat.jpg')
 
-    @mock.patch('sqlalchemy_media.stores.gcp.storage')
+    @mock.patch('sqlalchemy_media.stores.GoogleCloudStorage.storage')
     def test_delete(self, mock_storage: storage):
         mock_storage.Client.return_value = Mock()
         mock_storage.Client.return_value.get_bucket.return_value = Mock()
